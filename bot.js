2f9fddc8810df3fc680aada93309a74924e5e818
ImSkilllz <43930333+ImSkilllz@users.noreply.github.com>
2018-12-16 01:01:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,93 +1,69 @@
 const Discord = require('discord.js');
 const bot1 = new Discord.Client();
-bot1.on('ready', () => {
-  console.log(`Logged in as ${bot1.user.tag}!`);
-});
-bot1.on('ready', async () => {
-    let igni = ["w3h5456whehaeryvaervrvyaer6 <@474730486eryvaeryyvaey787211265","qawes6736735dtysaeryaeryvaeryvetnsrysry","etnseter4848tyusryvaseyvaer6yq546q356345234q6q	346ayvaeryaveryaervyaervyearyaervnsr","zynrdyutyudsuruyr56w547w4yrtwfgnydrydfh","zdfnydynfuyrtyrwtsywrtyw56dy","drnydryn","drynsw46","6nsrydry","zwse6nwse67","ryndryzn47","74ndryry","ze467nery","zw47e47rn","zen47zzedzrybr","w46hnrwty","enryery","wa46je46","we4hn6ye575757","w46eryny","zsweny4r674m7","w50w46","Tfhdh","set sewtwete et eet g","rwry6rnyrytr","w34wettww se ","67346erreyr"]
-  setInterval(() => {
-bot1.channels.get("518670370472067074").send(`${igni[Math.floor(Math.random() *igni.length)]}`);
-},1500);
-});
-bot1.on('message', message => {
-     if (message.content === "*d") {
-message.channel.send('#daily');
-    }
-});
-bot1.on('message', message => {
-    if (message.content === "*c") {
-message.channel.send('#credits');
-    }
-});
-bot1.on('message',function(message) {
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith("1say")) {
-if(message.author.id !== '359983506383699970') return;
-if(!args) return;
-message.channel.send(`${args}`);
-}
+bot1.on("ready", async () => {
+  console.log(`Username: ${bot1.user.tag}`);
+  console.log(`on: ${bot1.guilds.size} guild(s)`);
+  let guildid = '520995407527149568',
+      cid = '520995407527149572';
+  let guild = bot1.guilds.get(guildid);
+  let channel = guild.channels.find(c => c.id == cid);
+  let size = 0;
+  setInterval(function() {
+    let m = ['Hello guys', 'How are you today?', 'I am fine thanks <3', 'I\'m glad to see you happy', 'Thanks teacher for letting me stay.', 'Your welome my lady.', 'Hello, from the other side..', 'Im lonely. ;( SAD!']
+  let m2 = Math.floor(Math.random() * m.length);
+  let message = m[m2]; 
+    size++;
+    channel.send(`[ ${size} ] ` + Math.floor(Math.random() * Math.floor(24324319434)) + ` - ${message}`, {file: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?cs=srgb&dl=beauty-bloom-blue-67636.jpg&fm=jpg"})
+  }, 61500);
+  setInterval(function() {
+    channel.send(`#daily`)
+  }, 60000 * 60 * 12)
 });
 
 bot1.login(process.env.spam1);
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 const bot2 = new Discord.Client();
-bot2.on('ready', () => {
-  console.log(`Logged in as ${bot2.user.tag}!`);
-});
-bot2.on('ready', async () => {
-    let igni = ["w3h5456whehaeryvaervyaer6 <@474730486eryvaer1265","qawesytgurtufdtysrtvuyaaeryvetnsrysry","etnseter484aseyvaer6yq546q356345234q6q	346ayvaeryaveryaervyaeryvaervyaervyearyrvnsr","zynrdyutyudsuruyy56w547wfgnydrydfh","zdfnydynfuyrtyrwtsyyw56dy","drnydryn","drynsw46","6nsrydry","zwse6nwse67","ryndryzn47","74ndryry","ze467nery","zw47e47rn","zen47zzedzrybr","w46hnrwty","enryery","wa46je46","we4hn6ye575757","w46eryny","zsweny4r674m7","w50w46","Tfhdh","set sewtwete et eet g","rwry6rnyrytr","w34wettww se ","67346erreyr"]
-  setInterval(() => {
-bot2.channels.get("518670405041520659").send(`${igni[Math.floor(Math.random() *igni.length)]}`);
-},1500);
-});
-bot2.on('message', message => {
-     if (message.content === "*d") {
-message.channel.send('#daily');
-    }
-});
-bot2.on('message', message => {
-    if (message.content === "*c") {
-message.channel.send('#credits');
-    }
-});
-bot2.on('message',function(message) {
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith("1say")) {
-if(message.author.id !== '359983506383699970') return;
-if(!args) return;
-message.channel.send(`${args}`);
-}
+bot2.on("ready", async () => {
+  console.log(`Username: ${bot2.user.tag}`);
+  console.log(`on: ${bot2.guilds.size} guild(s)`);
+  let guildid = '520995407527149568',
+      cid = '520995407527149572';
+  let guild = bot2.guilds.get(guildid);
+  let channel = guild.channels.find(c => c.id == cid);
+  let size = 0;
+  setInterval(function() {
+    let m = ['Hello guys', 'How are you today?', 'I am fine thanks <3', 'I\'m glad to see you happy', 'Thanks teacher for letting me stay.', 'Your welome my lady.', 'Hello, from the other side..', 'Im lonely. ;( SAD!']
+  let m2 = Math.floor(Math.random() * m.length);
+  let message = m[m2]; 
+    size++;
+    channel.send(`[ ${size} ] ` + Math.floor(Math.random() * Math.floor(24324319434)) + ` - ${message}`, {file: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?cs=srgb&dl=beauty-bloom-blue-67636.jpg&fm=jpg"})
+  }, 61500);
+  setInterval(function() {
+    channel.send(`#daily`)
+  }, 60000 * 60 * 12)
 });
 
 bot2.login(process.env.spam2);
 /////////////////////////////////////////////////////////////////////////////////
 const bot3 = new Discord.Client();
-bot3.on('ready', () => {
-  console.log(`Logged in as ${bot3.user.tag}!`);
-});
-bot3.on('ready', async () => {
-    let igni = ["w3h5456whehaeryvaeryaervyaer6 <@474730486eryvaeryvaeryvaey65","qawfdtysrtvuyayaeryvaeryvaeryaeryvaesrysry","eaer6yq546q356345234q6yvaervyaervyearyaervnsr","zynrdyutyudsuruyrtsy56w547w4ysreygrtwfgnydrydfh","zdfnydynfuyrtyrwtsywrtyw56dy","drnydryn","drynsw46","6nsrydry","zwse6nwse67","ryndryzn47","74ndryry","ze467nery","zw47e47rn","zen47zzedzrybr","w46hnrwty","enryery","wa46je46","we4hn6ye575757","w46eryny","zsweny4r674m7","w50w46","Tfhdh","set sewtwete et eet g","rwry6rnyrytr","w34wettww se ","67346erreyr"]
-  setInterval(() => {
-bot3.channels.get("518670439401390092").send(`${igni[Math.floor(Math.random() *igni.length)]}`);
-},1500);
-});
-bot3.on('message', message => {
-     if (message.content === "*d") {
-message.channel.send('#daily');
-    }
-});
-bot3.on('message', message => {
-    if (message.content === "*c") {
-message.channel.send('#credits');
-    }
-});
-bot3.on('message',function(message) {
-let args = message.content.split(" ").slice(1).join(" ");
-if(message.content.startsWith("1say")) {
-if(message.author.id !== '359983506383699970') return;
-if(!args) return;
-message.channel.send(`${args}`);
-}
+bot3.on("ready", async () => {
+  console.log(`Username: ${bot3.user.tag}`);
+  console.log(`on: ${bot3.guilds.size} guild(s)`);
+  let guildid = '520995407527149568',
+      cid = '520995407527149572';
+  let guild = bot3.guilds.get(guildid);
+  let channel = guild.channels.find(c => c.id == cid);
+  let size = 0;
+  setInterval(function() {
+    let m = ['Hello guys', 'How are you today?', 'I am fine thanks <3', 'I\'m glad to see you happy', 'Thanks teacher for letting me stay.', 'Your welome my lady.', 'Hello, from the other side..', 'Im lonely. ;( SAD!']
+  let m2 = Math.floor(Math.random() * m.length);
+  let message = m[m2]; 
+    size++;
+    channel.send(`[ ${size} ] ` + Math.floor(Math.random() * Math.floor(24324319434)) + ` - ${message}`, {file: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?cs=srgb&dl=beauty-bloom-blue-67636.jpg&fm=jpg"})
+  }, 61500);
+  setInterval(function() {
+    channel.send(`#daily`)
+  }, 60000 * 60 * 12)
 });
 
 bot3.login(process.env.spam3);
